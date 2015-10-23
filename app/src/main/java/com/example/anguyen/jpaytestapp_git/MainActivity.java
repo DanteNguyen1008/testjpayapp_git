0cf3b8695f94fd465dabf64920be6ff9e3fdf7aa
anguyen <anguyen@jpay.com>
2015-10-23 09:49:02 -0400

    Trigger Jenkins build

--- app/src/main/java/com/example/anguyen/jpaytestapp_git/MainActivity.java
+++ app/src/main/java/com/example/anguyen/jpaytestapp_git/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        /*making change to trigger jenkins build*/
+        Log.d("MainActivity", "Trigger Jenkins build");
     }
 
     @Override
